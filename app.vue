26c908f0ac038c368bccef276dfca537b2e5e20d
Dejan Dimic <dejan.dimic@gmail.com>
2024-03-18 13:53:10 +0100

    feat(project): add google fonts

--- app.vue
+++ app.vue
@@ -3,3 +3,9 @@
     <h1>Welcome to OMA KNOWLEDGE BASE</h1>
   </UContainer>
 </template>
+
+<style>
+body {
+  font-family: "DM Sans", serif;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      "DM Sans": [400, 500, 600, 700],
+      "DM Mono": [400, 500],
+    },
+  },
   devtools: { enabled: true },
 });
