e9b79e47ce692e5b5a75ba40c68b6dd89bb37093
Dejan Dimic <dejan.dimic@gmail.com>
2024-06-02 00:22:03 +0200

    feat(components): add ShMultiColumn component

---
+++ components/content/sh-multi-column.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="ui.wrapper">
+    <div :class="gridClass">
+      <ContentSlot :use="$slots.default" unwrap="" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {multiColumn as config, gridSizes } from "@/ui.config"
+  
+const props = withDefaults(
+  defineProps<{
+    ui?: Partial<typeof config>;
+    cols: Number;
+    gap?: String;
+    class?: Any;
+  }>(),
+  {
+    ui: () => ({}),
+    cols: () => config.default.cols,
+    gap: () => config.default.gap,
+    class: () => undefined
+  });
+
+const { ui, attrs } = useUI(
+  "sh-multi-column",
+  toRef(props, "ui"),
+  config,
+  toRef(props, "class")
+)
+
+const gridClass = computed(() => {
+  const cols = props.cols != undefined ? props.cols : config.default.cols
+
+  return ["grid", gridSizes.gridCols[cols], gridSizes.gridRows[1], props.gap].join(' ')
+})
+</script>
