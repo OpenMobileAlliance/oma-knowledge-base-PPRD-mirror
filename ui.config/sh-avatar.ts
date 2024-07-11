99bed0f43413825c8c182ea148d0dbbe15eeac2f
Filip Dragović <dr.ipi95@gmail.com>
2024-07-11 02:46:39 +0200

    update: updated ShAvatar for dark mode

--- ui.config/sh-avatar.ts
+++ ui.config/sh-avatar.ts
@@ -1,12 +1,12 @@
 export default {
-    wrapper: "grid grid-cols-2 gap-4 p-4 mx-auto w-fit",
+    wrapper: "grid grid-cols-2 gap-4 p-4 mx-auto w-fit dark:bg-white rounded-md",
     avatar: "grayscale justify-self-end tracking-widest rounded-full size-48",
     base: "",
-    name: "text-3xl font-bold grow text-oma-500 bottom-0",
-    role: "text-lg font-semibold text-black dark:text-white -mt-4",
-    company: "text-lg text-gray-500 tracking-widest -mt-5",
+    name: "text-3xl font-bold grow text-oma-500 bottom-0 dark:invert",
+    role: "text-lg font-semibold text-black dark:text-white -mt-4 dark:invert",
+    company: "text-lg text-gray-500 tracking-widest -mt-5 dark:text-black dark:invert",
     socials: "flex text-5xl -mx-1 -mt-4",
-    icon: "transition-transform transform hover:-translate-y-1 dark:text-white duration-400",
+    icon: "transition-transform transform hover:-translate-y-1 dark:text-black duration-400",
     // Default Tailwind CSS values
     default: {
     }
