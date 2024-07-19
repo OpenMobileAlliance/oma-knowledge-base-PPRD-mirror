311354e29ff0e01fdb7413d3187303a62d3be127
Filip Dragović <dr.ipi95@gmail.com>
2024-07-19 05:44:51 +0200

    fix: content wrapped by the border of consturctor

--- ui.config/sh-card.ts
+++ ui.config/sh-card.ts
@@ -1,14 +1,14 @@
 export default {
-  wrapper: "flex flex-col justify-between border-2 rounded-xl mx-auto max-w-lg h-screen",
-  upperBase: "flex items-center justify-center rounded-t-xl",
-  image: "h-full w-auto p-8",
-  lowerBase: "p-6 -my-6 rounded-b-xl",
+  wrapper: "flex flex-col justify-between border-2 rounded-xl mx-auto max-w-lg overflow-hidden",
+  upperBase: "flex items-center justify-center rounded-t-xl overflow-hidden",
+  image: "h-full w-auto p-8 object-cover",
+  lowerBase: "p-6 rounded-b-xl",
   title: "text-3xl font-extrabold",
   subtitle: "text-xl font-semibold text-gray-600 dark:text-gray-400 -mt-8",
-  text: "",
-  leftLabel: "text-lg justify-self-start text-gray-600 dark:text-gray-400",
-  centerLabel: "text-lg justify-self-center text-gray-600 dark:text-gray-400",
-  rightLabel: "text-lg justify-self-end text-gray-600 dark:text-gray-400",
+  text: "text-base text-gray-800 dark:text-gray-200",
+  leftLabel: "text-lg text-gray-600 dark:text-gray-400",
+  centerLabel: "text-lg text-gray-600 dark:text-gray-400",
+  rightLabel: "text-lg text-gray-600 dark:text-gray-400",
   // Default Tailwind CSS values
   default: {
   }
