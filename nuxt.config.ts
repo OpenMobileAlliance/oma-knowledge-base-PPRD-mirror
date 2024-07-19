788dc6292d0f22f5b9574ad1e852eb5489810364
Dejan Dimic <dejan.dimic@gmail.com>
2024-07-19 20:10:05 +0200

    feat: add dark mode switch button

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxt/content", "@nuxt/ui", "@nuxtjs/google-fonts", "nuxt-testimonial"],
+  colorMode: {
+    preference: 'light'
+  },
   content: {
     highlight: {
       theme: 'github-dark'
