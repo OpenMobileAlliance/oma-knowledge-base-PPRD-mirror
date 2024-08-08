8f83796793ae5aa8a63497688c7b2b6b8e585a46
Joaquin <jprado@standardshub.io>
2024-08-08 19:29:26 +0200

    updates to shlistcards style

--- ui.config/sh-list-cards.ts
+++ ui.config/sh-list-cards.ts
@@ -3,7 +3,7 @@ export default {
     header: "",
     title: "text-3xl font-extrabold text-center",
     subtitle: "text-xl font-semibold text-gray-600 dark:text-gray-400 -mt-8 text-center",
-    text: "text-center",
+    text: "text-center font-light",
     base: "",
     // Default Tailwind CSS values
     default: {
