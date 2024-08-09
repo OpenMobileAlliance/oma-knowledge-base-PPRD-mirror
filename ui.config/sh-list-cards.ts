0b738e1b098493d1269f1915ae65937e0c28da0d
Filip Dragović <dr.ipi95@gmail.com>
2024-08-09 13:18:14 +0200

    update: introduced responsivnes

--- ui.config/sh-list-cards.ts
+++ ui.config/sh-list-cards.ts
@@ -1,10 +1,10 @@
 export default {
-    wrapper: "",
+    wrapper: "pl-4 pr-4",
     header: "",
-    title: "text-3xl font-extrabold text-center",
-    subtitle: "text-xl font-semibold text-gray-600 dark:text-gray-400 -mt-8 text-center",
-    text: "text-center font-light",
-    base: "",
+    title: "pt-12 text-3xl font-extrabold text-center",
+    subtitle: "pb-6 text-xl font-semibold text-gray-600 dark:text-gray-400 -mt-8 text-center",
+    text: "pb-16 text-center font-light",
+    base: "justify-between flex flex-wrap",
     // Default Tailwind CSS values
     default: {
     }
