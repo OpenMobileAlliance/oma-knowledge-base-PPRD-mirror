bc62502737d49a2b552b9ad1980f87d28f6ea80f
Filip Dragović <dr.ipi95@gmail.com>
2024-08-15 14:59:32 +0200

    fix: next button navigation now directs to next content

--- components/prev-next-page.vue
+++ components/prev-next-page.vue
@@ -9,7 +9,7 @@ const { prev, next } = useContent()
         <div class="text-3xl font-semibold mt-2">{{ prev.title }}</div>
         <div v-if="prev.description" class="text-lg font-normal text-gray-500 dark:text-gray-400 mt-2">{{ prev.description }}</div>
     </NuxtLink>
-    <NuxtLink v-if="prev" :to="prev._path" class="grid grid-rows-3 border p-4 rounded-xl hover:bg-gray-100 dark:hover:bg-neutral-800 justify-items-end lg:ml-20 sm:ml-5">
+    <NuxtLink v-if="next" :to="next._path" class="grid grid-rows-3 border p-4 rounded-xl hover:bg-gray-100 dark:hover:bg-neutral-800 justify-items-end lg:ml-20 sm:ml-5">
         <UIcon name="i-vaadin:chevron-circle-right-o" dynamic class="text-3xl hover:scale-105" />
         <div class="text-3xl font-semibold mt-2">{{ next.title }}</div>
         <div v-if="next.description" class="text-lg font-normal text-gray-500 dark:text-gray-400 mt-2">{{ next.description }}</div>
