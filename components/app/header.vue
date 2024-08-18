43bfece2a7ec24ad58518aa317153b17fe9336ff
Dejan Dimic <dejan.dimic@gmail.com>
2024-08-18 18:14:53 +0200

    fix: app header

--- components/app/header.vue
+++ components/app/header.vue
@@ -1,45 +1,49 @@
 <template>
   <header :class="ui.wrapper">
-    <UContainer :ui="{constrained: '',}" :class="ui.container">
-      <div :class="ui.left">
-        <slot name="left">
-          <NuxtLink :to="to" :aria-label="ariaLabel" :class="ui.logo">
-            <slot name="logo">
-              {{ title || "OMA Knowledge Base" }}
-            </slot>
-          </NuxtLink>
-        </slot>
-      </div>
-      <div :class="ui.center">
-        <div class="flex justify-between items-start mb-2">
-          <div :class="ui.right">
-            <ColorMode />
-            <span class="mr-2 text-sm">
-              <ULink to="signin" active-class="text-primary" class="hover:text-primary">Sign In</ULink>
-            </span>
-            <span class="mr-2 text-sm">
-              <ULink to="contactus" class="hover:text-primary">Contact us</ULink>
-            </span>
-            <slot name="right">
-              <AppSocialLinks />
-            </slot>
-          </div>
+    <UContainer :ui="{ constrained: '', }" class="flex flex-col">
+      <div :class="ui.container">
+        <div :class="ui.left">
+          <slot name="left">
+            <NuxtLink :to="to" :aria-label="ariaLabel" :class="ui.logo">
+              <slot name="logo">
+                {{ title || "OMA Knowledge Base" }}
+              </slot>
+            </NuxtLink>
+          </slot>
         </div>
-        <div>
-          <ul class="flex gap-1.5">
-            <li v-for="link in topLinks" :key="link.path" class="ml-4" :style="{ fontFamily: header.menu.font.type, fontSize: header.menu.font.size }">
-              <ULink :to="link._path" class="hover:text-primary">{{ link.title }}</ULink>
-            </li>
-          </ul>
+        <div :class="ui.center">
+          <div class="flex justify-between items-start mb-2">
+            <div :class="ui.right">
+              <ColorMode />
+              <span class="mr-2 text-sm">
+                <ULink to="signin" active-class="text-primary" class="hover:text-primary">Sign In</ULink>
+              </span>
+              <span class="mr-2 text-sm">
+                <ULink to="contactus" class="hover:text-primary">Contact us</ULink>
+              </span>
+              <slot name="right">
+                <AppSocialLinks />
+              </slot>
+            </div>
+          </div>
+          <div>
+            <ul class="flex gap-1.5">
+              <li v-for="link in topLinks" :key="link.path" class="ml-4"
+                :style="{ fontFamily: header.menu.font.type, fontSize: header.menu.font.size }">
+                <ULink :to="link._path" class="hover:text-primary">{{ link.title }}</ULink>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </UContainer>
+    <AppBreadcrumbs class="mt-4" />
   </header>
 </template>
 
 <script setup lang="ts">
 const config = {
-  wrapper: "bg-background/75 backdrop-blur border-b border-gray-200 dark:border-gray-800 -mb-px sticky top-0 z-50",
+  wrapper: "bg-background/75 backdrop-blur -mb-px sticky top-0 z-50",
   container: "flex items-center justify-between gap-3 h-[--header-height]",
   left: "lg:flex-1 flex items-center gap-1.5",
   center: "hidden lg:flex flex flex-col grow",
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="h-screen">
-    <AppHeader class="relative py-4" title="OMA">
+    <AppHeader class="py-4" title="OMA">
       <template v-slot:logo>
         <img :src="computedLogoSrc" alt="Logo" />
       </template>
     </AppHeader>
-    <AppBreadcrumbs class="absolute z-20" />
     <UContainer :ui="{ constrained: '', }" class="pb-20">
       <slot />
       <PrevNextPage v-if="$route.path !== '/'" />
