d274eac41161f5c535d3dad02b5d854f61d366fe
Joaquin <jprado@standardshub.io>
2024-09-05 16:01:09 +0200

    updates

--- ui.config/sh-social.ts
+++ ui.config/sh-social.ts
@@ -2,7 +2,7 @@ export default {
     wrapper: "flex p-4 justify-center",
     facebook: "bg-white dark:bg-white rounded-xl border",
     linkedin: "",
-    twitter: "bg-white dark:bg-white hover:border-[#81C1B0] hover:border-4",
+    twitter: "bg-white dark:bg-white",
     // Default Tailwind CSS values
     default: {
     }
