c023dc5099c8203b25ca08f49c6d0d1acb630adc
Filip Dragović <dr.ipi95@gmail.com>
2024-09-17 04:01:14 +0200

    update: additional visual updates

--- pages/index.vue
+++ pages/index.vue
@@ -8,46 +8,43 @@
           :class="{ 'filter grayscale': isGrayscale, 'filter grayscale-0': !isGrayscale }" />
       </div>
     </div>
-
-    <div v-if="isTextVisible"
-      class="text-center font-serif text-6xl mt-16 text-neutral-700 dark:text-[#333333] transition-opacity duration-[2000ms] opacity-100 ease-in">
+    <div
+      :class="['text-center font-serif text-6xl mt-16 text-neutral-700 dark:text-[#333333] opacity-0', { 'transition-opacity duration-[1700ms] opacity-100 ease-in': !isGrayscale }]">
       Welcome to OMA KnowledgeBase
     </div>
-
     <div
-      :class="['content-center mt-[15%] ml-[120%] w-[20%] h-[10%] bg-neutral-400 dark:bg-neutral-600 rounded-l-full hover:scale-105 duration-700 delay-0', { 'transition-all -translate-x-full duration-1000 ease-in-out transform-gpu': isTextVisible }]">
+      :class="['content-center mt-[15%] ml-[120%] w-[20%] h-[10%] bg-neutral-400 dark:bg-neutral-600 rounded-l-full hover:scale-105 duration-700 delay-0 p-3', { 'transition-all -translate-x-full duration-1000 ease-in-out transform-gpu': isTextVisible }]">
       <NuxtLink to="/omaspecworks/about/" class="not-prose flex items-center">
-        <img src="/logo-dark.png" alt="OMA Logo" class="mx-auto" />
-        <UIcon name="i-line-md:home-twotone" dynamic class="text-4xl text-oma-blue-500 mr-6 mb-2 hover:contrast-125" />
+        <img v-if="opacity !== 'sm:opacity-0'" src="/logo-dark.png" alt="OMA Logo" :class="['mx-auto', opacity]" />
+        <UIcon name="i-line-md:home-twotone" dynamic
+          :class="['mx-auto text-4xl text-oma-blue-500 mr-6 mb-2 hover:contrast-125', { 'w-full h-full': opacity === 'sm:opacity-0' }]" />
       </NuxtLink>
     </div>
-
-    <div class="flex justify-center">
+    <div class="flex justify-center -mt-16">
       <UIcon name="i-iconoir:mouse-scroll-wheel" dynamic
         class="text-6xl text-neutral-700 transition-opacity duration-1000 delay-700 opacity-0 animate-bounce rounded-full hover:cursor-none"
         :class="{ 'opacity-100': isTextVisible }" />
     </div>
   </div>
 
-  <div class="h-[20%] bg-oma-blue-500 dark:bg-oma-blue-800 text-xl text-white p-44 text-center">
+  <div class="constructor h-[20%] bg-oma-blue-500 dark:bg-oma-blue-800 text-xl text-white p-44 text-center">
     <p>Welcome to OMA KnowledgeBase website, where all of our sites will be combined into one place.</p>
   </div>
 
-  <div class="h-screen bg-gradient-to-t from-oma-blue-500 p-16">
+  <div class="constructor h-screen bg-gradient-to-t from-oma-blue-500 p-16">
     <ContentDoc />
     <img src="/logo-dark.png" alt="OMA Logo" class="mx-auto mt-12 contrast-125" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, nextTick } from 'vue';
+import { ref, onMounted, computed, nextTick } from 'vue';
 
 const isLogoVisible = ref(false);
 const isGrayscale = ref(true);
 const isTextVisible = ref(false);
 
 onMounted(() => {
-
   setTimeout(() => {
     isLogoVisible.value = true;
   }, 700);
@@ -56,11 +53,29 @@ onMounted(() => {
     isGrayscale.value = false;
   }, 1100);
 
-  nextTick();
-  setTimeout(() => {
-    isTextVisible.value = true;
-  }, 2000);
+  nextTick(() => {
+    setTimeout(() => {
+      isTextVisible.value = true;
+    }, 2000);
+  });
+
+  nextTick(() => {
+    const constructors = document.querySelectorAll('.constructor');
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        entry.target.classList.toggle('show', entry.isIntersecting);
+      });
+    }, {
+      threshold: 1,
+    });
+
+    constructors.forEach((constructor) => {
+      observer.observe(constructor);
+    });
+  })
 });
+
+const opacity = computed(() => 'sm:opacity-0 lg:opacity-100');
 </script>
 
 <style>
@@ -74,5 +89,16 @@ html {
 ::-webkit-scrollbar {
   width: 0;
   height: 0;
+}
+
+.constructor {
+  transform: translateX(100%);
+  opacity: 0;
+  transition: 1000ms;
+}
+
+.constructor.show {
+  transform: translateX(0);
+  opacity: 1;
 }
 </style>
