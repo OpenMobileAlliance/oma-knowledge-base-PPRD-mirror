c2c8f577a738c8abbf06ae43decff161ed7ed0a3
Filip Dragović <dr.ipi95@gmail.com>
2024-10-08 03:06:30 +0200

    update: mt class change for light mode

--- components/content/sh-helper-component.vue
+++ components/content/sh-helper-component.vue
@@ -5,7 +5,7 @@
           <img src="/images/oma2.png" class="w-full h-full md:h-44" />
         </div>
         <div class="p-7">
-          <img v-if="!computedLogoSrc" src="/logo-light.png" class="w-full h-full md:h-32 mt-12" />
+          <img v-if="!computedLogoSrc" src="/logo-light.png" class="w-full h-full md:h-32 mt-20" />
           <img v-if="computedLogoSrc" src="/logo-dark.png" class="w-full h-full md:h-32 mt-20" />
         </div>
       </div>
