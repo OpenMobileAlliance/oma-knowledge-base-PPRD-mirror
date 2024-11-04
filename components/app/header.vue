8b70bf169212cb7ba4078456308df7eceaa548df
Filip Dragovic <128386451+FroudeDescartes@users.noreply.github.com>
2024-11-04 23:52:43 +0100

    [WIP: DO NOT MERGE] Header menu (#308)
    * update: style
    
    * update: added signIn icon
    
    * update: added links for sign option

--- components/app/header.vue
+++ components/app/header.vue
@@ -16,14 +16,23 @@
           <div class="flex justify-between items-start mb-2">
             <div :class="ui.right">
               <ColorMode />
-              <span class="mr-2 text-base">
+              <span class="mr-2 text-base mt-1">
                 <!-- TODO: provide proper URL -->
-                <ULink to="/" active-class="text-primary" class="text-black dark:text-golden hover:text-primary">Sign In
-                </ULink>
+                <UDropdown :items="items" mode="hover" :popper="{ placement: 'bottom-start' }"
+                  :ui="{ background: 'bg-golden/[0.5] dark:bg-neutral-600', item: { active: 'bg-golden saturate-150 dark:bg-golden/[0.2]' } }">
+                  <template v-slot:item="{ item }">
+                    <ULink :to="item.to" class="text-black hover:text-black dark:text-golden hover:dark:text-golden flex items-center">
+                      <UIcon :name="item.icon" class="text-2xl mr-2" />
+                      {{ item.label }}
+                    </ULink>
+                  </template>
+                  <UIcon name="stash:user-avatar" dynamic class="text-3xl" />
+                </UDropdown>
+
               </span>
-              <span class="hidden sm:flex mr-2 text-base">
+              <!-- <span class="hidden sm:flex mr-2 text-base">
                 <ULink to="/contact-us" class="text-black dark:text-golden hover:text-primary">Contact us</ULink>
-              </span>
+              </span> -->
               <slot name="right">
                 <AppSocialLinks class="text-xl hidden sm:flex" />
               </slot>
@@ -47,19 +56,20 @@
           <!-- Dropdown for smaller screens -->
           <div class="2xl:hidden lg:mx-0">
             <UDropdown mode="click" :popper="{ placement: 'bottom-start' }">
-              <button @click="toggleDropdown"
-                class="bg-inherit hover:bg-primary-200/[0.7] dark:hover:bg-primary-600 text-xl lg:text-2xl text:black dark:text-golden p-2 pl-2 pr-2 rounded-2xl">
+              <button @click="toggleDropdownAndRotation"
+                class="bg-inherit hover:bg-primary-200/[0.7] dark:hover:bg-primary-600 text-xl lg:text-2xl text-black dark:text-golden p-2 pl-2 pr-2 rounded-2xl">
                 Menus
-                <UIcon name="gridicons:chevron-down" dynamic />
+                <UIcon :name="'gridicons:chevron-right'" dynamic
+                  :class="[isDropdownVisible ? 'rotate-90' : '', 'transition-transform', 'duration-500', 'ease-in-out']" />
               </button>
               <ul v-if="isDropdownVisible"
-                class="absolute right-0 mt-11 flex flex-col gap-1.5 bg-gray-100 dark:bg-neutral-700 p-5 rounded-xl shadow-md z-10">
-                <li v-for="(link, index) in topLinks" :key="link.path" class="ml-4"
-                  :style="{ fontFamily: header.menu.font.type, fontSize: header.menu.font.size }">
+                class="background absolute right-0 mt-11 flex flex-col gap-1.5 p-5 rounded-xl shadow-md z-10">
+                <li v-for="(link, index) in topLinks" :key="link.path" class="text-lg lg:text-xl"
+                  :style="{ fontFamily: header.menu.font.type }">
                   <div v-if="index !== 0" class="p-3 mb-2">
                     <hr />
                   </div>
-                  <ULink @click="toggleDropdown" :to="link._path" :class="ui.shadow">
+                  <ULink @click="toggleDropdownAndRotation" :to="link._path" :class="ui.shadow">
                     {{ link.title }}
                   </ULink>
                 </li>
@@ -82,7 +92,7 @@ const config = {
   center: "flex flex-col grow",
   right: "flex items-center lg: lg:justify-end lg:flex-1 gap-1.5",
   logo: "flex-shrink-0 font-bold text-xl text-gray-900 dark:text-white flex items-end gap-1.5",
-  shadow: "text:black dark:text-golden hover:text-black dark:hover:text-golden hover:bg-primary-200 dark:hover:bg-primary-600 rounded-xl p-2",
+  shadow: "text:black dark:text-golden hover:text-black dark:hover:text-golden hover:bg-golden dark:hover:bg-primary-600 rounded-xl p-2",
 };
 
 const props = withDefaults(
@@ -108,10 +118,21 @@ const { ui, attrs } = useUI(
   true,
 );
 
+const items = [
+  [{
+    label: 'Login',
+    icon: 'i-heroicons-user',
+    to: 'https://member.openmobilealliance.org/'
+  }], [{
+    label: 'Create an account',
+    icon: 'i-heroicons-user-plus',
+    to: 'http://member.openmobilealliance.org/AccountCreation/GetAccount.asp'
+  }]
+]
+
 const isDropdownVisible = ref(false);
 
-// Function to toggle dropdown visibility
-const toggleDropdown = () => {
+const toggleDropdownAndRotation = () => {
   isDropdownVisible.value = !isDropdownVisible.value;
 };
 
@@ -135,3 +156,13 @@ function isLinkActive(path: string): boolean {
   return route.path.startsWith(path);
 }
 </script>
+
+<style scoped>
+.background {
+  background-color: rgb(242, 235, 226);
+}
+
+.dark .background {
+  @apply bg-neutral-700
+}
+</style>
