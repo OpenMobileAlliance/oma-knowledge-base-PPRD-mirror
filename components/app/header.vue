c33e7f79b0835f0249a8f9e8763427ed2dd37213
Filip Dragovic <128386451+FroudeDescartes@users.noreply.github.com>
2024-11-06 11:09:29 +0100

    Issue 352 (#367)
    * update: ToC better responsivnes
    
    * fix: bg login non transparent

--- components/app/header.vue
+++ components/app/header.vue
@@ -19,7 +19,7 @@
               <span class="mr-2 text-base mt-1">
                 <!-- TODO: provide proper URL -->
                 <UDropdown :items="items" mode="hover" :popper="{ placement: 'bottom-start' }"
-                  :ui="{ background: 'bg-golden/[0.5] dark:bg-neutral-600', item: { active: 'bg-golden saturate-150 dark:bg-golden/[0.2]' } }">
+                  :ui="{ background: 'bg-golden contrast-[90%] dark:bg-neutral-600', item: { active: 'bg-golden saturate-150 dark:bg-golden/[0.2]' } }">
                   <template v-slot:item="{ item }">
                     <ULink :to="item.to" target="_blank" class="text-black hover:text-black dark:text-golden hover:dark:text-golden flex items-center">
                       <UIcon :name="item.icon" class="text-2xl mr-2" />
