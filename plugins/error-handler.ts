e3170710ff33d13d70d4f4293931b0901297b038
Dejan Dimic <dejan.dimic@gmail.com>
2025-09-12 23:27:45 +0200

    fix: change error handler to show 404 page

--- plugins/error-handler.ts
+++ plugins/error-handler.ts
@@ -1,7 +1,7 @@
 export default defineNuxtPlugin(nuxtApp => {
   nuxtApp.hook('app:error', (err) => {
     console.log(`APP ERROR: ${err}`)
-    clearError({ redirect: '/home' })
+    clearError({ redirect: '/404' })
     return false
   })
 })
